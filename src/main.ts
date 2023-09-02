644de695001d1e10f04a356c5833d347e4204fd0
SalemC <salem.cresswell@btinternet.com>
2023-09-02 17:44:51 +0100

    feat(main): Update parsed syntax

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,16 @@
-import { Lexer } from './Lexer';
-import { Parser } from './Parser';
-
 import util from 'util';
 
+import { Parser } from './Parser';
+import { Lexer } from './Lexer';
+
 const lexer = new Lexer();
+const parser = new Parser();
 
 const tokens = lexer.convertToTokens(`
     const value = 420;
 
-    return value;
+    terminate(value);
 `);
-
 console.log(tokens);
-
-const parser = new Parser();
-
 const statements = parser.parseTokens(tokens);
-
 console.log(util.inspect(statements, { showHidden: false, depth: null, colors: true }));
